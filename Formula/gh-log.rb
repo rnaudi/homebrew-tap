2e8ef193437c8b7fd58f215f20e67b9e5ca68bdb
rnaudi <arnau.diaz@scopely.com>
2026-01-07 15:25:30 +0100

    Fix sha256 checksum formatting in gh-log.rb

--- Formula/gh-log.rb
+++ Formula/gh-log.rb
@@ -5,12 +5,12 @@ class GhLog < Formula
   
   on_macos do
     url "https://github.com/rnaudi/gh-log/releases/download/v0.1.5/gh-log-aarch64-apple-darwin.tar.gz"
-    sha256 "sha256:caa53698502c94a3d0da7df35c8403f9d6c6eb32e442787d0bd6aef48518bf14"
+    sha256 "caa53698502c94a3d0da7df35c8403f9d6c6eb32e442787d0bd6aef48518bf14"
   end
 
   on_linux do
     url "https://github.com/rnaudi/gh-log/releases/download/v0.1.5/gh-log-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "sha256:caa53698502c94a3d0da7df35c8403f9d6c6eb32e442787d0bd6aef48518bf14"
+    sha256 "caa53698502c94a3d0da7df35c8403f9d6c6eb32e442787d0bd6aef48518bf14"
   end
 
   def install
