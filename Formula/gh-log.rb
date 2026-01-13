c661e084af3cdadd035ba270063445795a443c97
rnaudi <arnau.diaz@scopely.com>
2026-01-13 10:40:09 +0100

    gh-log 0.1.10
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/gh-log.rb
+++ Formula/gh-log.rb
@@ -1,11 +1,11 @@
 class GhLog < Formula
   desc "GitHub log viewer CLI"
   homepage "https://github.com/rnaudi/gh-log"
-  version "0.1.9"
+  version "0.1.10"
   
   on_macos do
-    url "https://github.com/rnaudi/gh-log/releases/download/v0.1.9/gh-log-aarch64-apple-darwin.tar.gz"
-    sha256 "fabaa2d988ed0d66d2e313a4a4e6b7c7ad9bc403f21f378489e4af66fa59b823"
+    url "https://github.com/rnaudi/gh-log/releases/download/v0.1.10/gh-log-aarch64-apple-darwin.tar.gz"
+    sha256 "a40c5bea5608f575ae9d78821e20d6b135d6f5566bf13876b9340e658530b23c"
   end
 
   on_linux do
